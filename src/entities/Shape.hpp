1c0ca20d64289ee377cff94d584a8fe84ef4fd84
TamWilliam <72071669+TamWilliam@users.noreply.github.com>
2025-11-05 10:50:13 +0100

    add: shape interface with hit

---
+++ src/entities/Shape.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/Ray.hpp"
+#include "core/HitRecord.hpp"
+
+class Shape {
+public:
+	virtual ~Shape() = default;
+
+	virtual bool hit(const Ray& ray, float tMin, float tMax, HitRecord& hitRecord) const = 0;
+};
